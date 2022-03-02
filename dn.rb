2472435949da8046646677197eb197678c8f02e8
EC2 Default User <ec2-user@ip-172-31-11-96.us-east-2.compute.internal>
2022-03-02 19:17:29 +0000

    Database operations

---
+++ dn.rb
@@ -0,0 +1,15 @@
+require "mysql2"
+
+host = "mydb1.cyzid7emtumx.us-east-2.rds.amazonaws.com"
+username = "Udemy"
+password = "Tandil2021"
+database = "ecsite"
+
+client = Mysql2::Client.new(host: host, username: username, database: database, password: password)
+
+
+ delete_results = client.query("DELETE  FROM products WHERE id=10")
+ p delete_results
+ 
+  
+
